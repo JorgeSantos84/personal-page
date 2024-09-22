5c8b7a9a1cce77c4901eb3730761d5cf16c8af0d
Jorge Santos <jorgesantos84@gmail.com>
2024-09-22 16:02:11 +0100

    first version of personal page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import { useEffect } from "react";
+import "./App.css";
+import Comentario from "./components/Comentario.jsx";
+import ContadorExercicio from "./components/ContadorExercicio";
+import todosComentarios from "./constants/TodosComentarios.js";
 
 function App() {
+  const apresentarComentarios = () => {
+    return todosComentarios.map((coment) => (
+      <Comentario
+        key={coment.id} // Add a unique key prop for each element
+        user={coment.userId} // Adjust if you need to convert to string
+        userId={coment.userId}
+        titulo={coment.title}
+        corpo={coment.body}
+      />
+    ));
+  };
+
+  // useEffect(() => {
+  //   fetch("http://jsonplaceholder.typicode.com/posts")
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data));
+  // }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
-        </a>
+        </a> */}
+
+        <ContadorExercicio />
+        {apresentarComentarios()}
+        <Comentario></Comentario>
       </header>
     </div>
   );
