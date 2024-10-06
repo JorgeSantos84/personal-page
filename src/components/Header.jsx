428c370010326c300e727c35bd9f08ed5bb00d78
Jorge Santos <jorgesantos84@gmail.com>
2024-10-06 21:27:49 +0100

    fix headbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,26 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
 import Avatar from "@mui/material/Avatar";
 import Switch from "@mui/material/Switch";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import MenuIntroduction from "./MenuIntroduction";
 import myAvatar from "../assets/avatar_jorge.jpg";
 import FlagsSelect from "react-flags-select";
 import { useTheme } from "../context/ThemeContext";
+import MenuIcon from "@mui/icons-material/Menu";
+import IconButton from "@mui/material/IconButton";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import Drawer from "@mui/material/Drawer";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import List from "@mui/material/List";
+import { useEffect } from "react";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import { Tooltip } from "@mui/material";
 
 const LanguageSelect = () => {
   const [selected, setSelected] = React.useState("PT"); // Default to Portugal
@@ -26,16 +34,60 @@ const LanguageSelect = () => {
         countries={["PT", "GB"]} // Include Portugal and UK flags
         placeholder="PT/EN"
         customLabels={{ PT: "PT", GB: "EN" }}
-        selectedSize={12}
+        selectedSize={14}
       />
     </>
   );
 };
 
 const Header = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const lightModeIcon = <LightModeIcon sx={{ color: "orange" }} />;
+  const darkModeIcon = <DarkModeIcon sx={{ color: "white" }} />;
+
   const [auth, setAuth] = React.useState(true);
   const [anchorEl, setAnchorEl] = React.useState(null);
   const { theme, toggleTheme } = useTheme();
+  const [themeIcon, setIconTheme] = React.useState(lightModeIcon);
+
+  useEffect(() => {
+    auth ? setIconTheme(lightModeIcon) : setIconTheme(darkModeIcon);
+  }, [auth]);
 
   const handleChange = (event) => {
     setAuth(event.target.checked);
@@ -51,76 +103,50 @@ const Header = () => {
   };
 
   return (
-    <Box>
-      <AppBar
-        position="fixed"
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        height: "80px",
+      }}
+    >
+      <Box>
+        <Avatar
+          alt="Jorge Santos"
+          src={myAvatar}
+          sx={{ width: 50, height: 50, marginLeft: 2 }}
+        />
+      </Box>
+      <Box
         sx={{
-          backgroundColor: "transparent",
-          boxShadow: "none",
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "row",
+          marginRight: 2,
         }}
       >
-        {/* Right-aligned Switch */}
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "flex-end",
-            padding: 1,
-            height: "10px",
-            marginTop: "7px",
-          }}
-        >
+        <Tooltip title="Mudar Tema" arrow>
           <FormControlLabel
-            style={{ color: "black" }}
             control={
-              <Switch
-                checked={auth}
-                onChange={handleChange}
-                aria-label="login switch"
-              />
+              <Switch checked={auth} onChange={handleChange} defaultChecked />
             }
-            label={
-              <Box
-                sx={{
-                  display: "inline-flex", // Use inline-flex for label
-                  alignItems: "center", // Center the text vertically
-                  justifyContent: "center", // Center the text horizontally
-                  backgroundColor: "black",
-                  borderRadius: "20px", // Round background
-                  padding: "1px 5px", // Padding for some space around text
-                  color: "white", // Text color
-                  marginLeft: "8px", // Space between switch and label
-                }}
-              >
-                {auth ? "Claro" : "Escuro"}
-              </Box>
-            }
+            label={themeIcon}
           />
-        </Box>
-
-        {/* Toolbar Section */}
-        <Toolbar
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-          }}
-        >
-          {/* Avatar on the left */}
-          <Box>
-            <Avatar
-              alt="Jorge Santos"
-              src={myAvatar}
-              sx={{ width: 50, height: 50, marginTop: "5px" }}
-            />
-          </Box>
-
-          <Box sx={{ display: "flex", alignItems: "center" }}>
-            <Box sx={{ marginRight: 2 }}>
-              <LanguageSelect></LanguageSelect>
-            </Box>
-            <MenuIntroduction /> {/* Use marginLeft instead of marginRight */}
-          </Box>
-        </Toolbar>
-      </AppBar>
+        </Tooltip>
+        <LanguageSelect></LanguageSelect>
+        <IconButton size="large">
+          <MenuIcon
+            fontSize="inherit"
+            sx={{ cursor: "pointer" }}
+            onClick={toggleDrawer(true)}
+            className="icons-theme"
+          />
+          <Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
+            {DrawerList}
+          </Drawer>
+        </IconButton>
+      </Box>
     </Box>
   );
 };
